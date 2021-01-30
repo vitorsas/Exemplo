683619f3b37f956f5a4f457733ab4e2aca40335a
vitorsas <vitor.sas@gmail.com>
2021-01-30 19:06:23 -0300

    Alpha

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_eigen.h>  
+#include <complex.h>        
+#include <fftw3.h>
+
+int main(int argc, char **argv){
+	printf("hello world\n");
+}
